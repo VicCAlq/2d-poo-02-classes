8592e3aab2c6f3e197a8d422f3fbdcdeb2e0a9d9
Wesley-Eder-Nave <wesley.eder@nave.org.br>
2026-07-03 16:43:08 -0300

    atv 01 em andamento

--- src/Atv01ClasseJogo.js
+++ src/Atv01ClasseJogo.js
@@ -15,3 +15,33 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+
+class Jogo {
+    constructor(
+        nomeDoJogo, tipo, qtdjogadores, dificuldade, fases
+    ) {
+        this.nomeDoJogo = nomeDoJogo
+        this.tipo = tipo
+        this.fasesConcluidas = 0
+        this.zerado = false
+    }
+}
+
+const jogo1 = new Jogo("Kirby", "ação", 2, 1, 20)
+const jogo2 = new Jogo("Sonic 1", "plataforma", 1, 3, 18)
+
+console.log(jogo1)
+
+Kirby {
+    nomeDoJogo: "Kirby",
+    tipo: "ação",
+    fasesConcluidas: 0,
+    zerado: false
+}
+
+Sonic 1 {
+    nomeDoJogo: "Sonic 1",
+    tipo: "plataforma",
+    fasesConcluidas: 0,
+    zerado: false
+}
