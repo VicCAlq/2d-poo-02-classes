81f239d2213296c59e3800b919d1c827bc9f090a
brendha-priscilla-nave <brendha.priscilla@nave.org.br>
2026-07-27 18:08:12 -0300

    Atualizar o Atv02ClasseJogoCompleto.js

--- src/Atv02ClasseJogoCompleto.js
+++ src/Atv02ClasseJogoCompleto.js
@@ -10,3 +10,31 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+
+class Jogo {
+constructor(nomeDoJogo,tipo, qtdJogadores, dificuldade, fases)
+{
+this.nomeDoJogo= nomeDoJogo 
+this.tipo= tipo
+this.qtdJogadores= qtdJogadores 
+this.dificuldade= dificuldade 
+this.fases= fases
+this.fasesConcluidas= 0
+this.zerado= false
+}
+
+jogar(minutos){
+
+let blocos = Math.floor(minutos/10);
+
+this.fasesConcluidas += blocos * (1/this.dificuldade);
+
+if(this.fasesConcluidas==this.fases){
+this.zerado = true};
+}
+
+}
+
+
+
+export default Jogo;
