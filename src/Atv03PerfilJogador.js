f3eb0ab89b2ac5ffec14ddde38b63a8f556efd1c
Jailson-Ferreira-nave <jailson.ferreira@nave.org.br>
2026-07-31 15:26:42 -0300

    sa

--- src/Atv03PerfilJogador.js
+++ src/Atv03PerfilJogador.js
@@ -12,3 +12,16 @@
  * Lembre-se de exportar a classe "PerfilJogador" no final do arquivo usando
  * "export default PerfilJogador"
  */
+
+class PerfilJogador {
+  constructor(jogador, email) {
+    this.jogador = String(jogador);
+    this.email = String(email);
+    
+   
+    this.jogos = [];  
+    this.amigos = []; 
+  }
+}
+
+export default PerfilJogador;
