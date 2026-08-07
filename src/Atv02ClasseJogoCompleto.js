7d6eb37f6bd287b5304bda4c8f90cfe7439c612b
Gabriel-Leal-Nave <gabriel.leal@nave.org.br>
2026-08-07 15:24:08 -0300

    feito

--- src/Atv02ClasseJogoCompleto.js
+++ src/Atv02ClasseJogoCompleto.js
@@ -10,3 +10,26 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+class Jogo {
+    constructor(nomeDoJogo, tipo, qtdJogadores, dificuldade, fases) {
+        this.nomeDoJogo = nomeDoJogo;
+        this.tipo = tipo;
+        this.qtdJogadores = qtdJogadores;
+        this.dificuldade = dificuldade;
+        this.fases = fases;
+        this.fasesConcluidas = 0;
+        this.zerado = false;
+    }
+
+    jogar(minutos) {
+        const fasesGanhas = Math.floor(minutos / 10) * (1 / this.dificuldade);
+        this.fasesConcluidas += fasesGanhas;
+
+        if (this.fasesConcluidas >= this.fases) {
+            this.fasesConcluidas = this.fases;
+            this.zerado = true;
+        }
+    }
+}
+
+export default Jogo;
