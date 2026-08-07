f384460ac3c08be9a0b356ed41f21554b91fc212
Arthur <arthur.machado@nave.org.br>
2026-08-07 16:37:02 -0300

    Enhance PerfilJogador class with validation and methods
    Added validation for constructor parameters and implemented jogar method logic.

--- src/Atv03PerfilJogador.js
+++ src/Atv03PerfilJogador.js
@@ -12,3 +12,37 @@
  * Lembre-se de exportar a classe "PerfilJogador" no final do arquivo usando
  * "export default PerfilJogador"
  */
+class PerfilJogador {
+  constructor(
+    jogador, email, jogos, amigos
+  ) {
+    if (typeof(jogador) !== "string" 
+        || typeof(email) !== "string"
+        || typeof(jogos) !== "string"
+        || typeof(amigos) !== "string"
+        ){
+            throw new Error("Tipo errado boy")
+        }
+            this.jogador = jogador
+            this.email = email
+            this.jogos = jogos
+            this.amigos = amigos
+            }
+    jogar(minutos){
+        while (minutos >= 0){
+            minutos =- 10
+            this.fasesConcluidas += 1/this.dificuldade
+        }
+        this.fasesConcluidas += (minutos/10)/this.dificuldade
+        if (this.fasesConcluidas == this.fases) {
+            this.zerado = true
+        }
+    }
+}
+
+const perfilJogador = new PerfilJogador(
+    "Rurthraa", "Arthurmachadocorreia0w0@gmail.com", 
+)
+
+console.log(PerfilJogador.jogar())
+export default PerfilJogador
