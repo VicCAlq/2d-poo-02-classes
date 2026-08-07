786e4880eea40a1f888aeb302d5471e81f61b9b8
Arthur <arthur.machado@nave.org.br>
2026-08-07 16:36:38 -0300

    Update Atv01ClasseJogo.js

--- src/Atv01ClasseJogo.js
+++ src/Atv01ClasseJogo.js
@@ -15,3 +15,38 @@
  * Lembre-se de exportar a classe "Jogo" no final do arquivo usando
  * "export default Jogo"
  */
+
+class Jogo {
+  constructor(
+    nomeDoJogo, tipo, qtdJogadores, dificuldade, fases, fasesConcluidas, zerado
+  ) {
+    if (typeof(nomeDoJogo) !== "string" 
+            || typeof(tipo) !== "string"
+            || typeof(qtdJogadores) !== "number"
+            || typeof(dificuldade) !== "number"
+            || dificuldade < 1 || dificuldade > 5
+            || typeof(fases) !== "number"
+            || typeof(fasesConcluidas) !== "number"
+            || typeof(zerado) !== "boolean")
+            {
+                console.log(`Os dados informados não estão no formato correto:
+                titulo: texto, tipo: texto, duracao: número, assitido: booleano ou vazio`)
+                return
+            }
+            this.nomeDoJogo = nomeDoJogo
+            this.tipo = tipo
+            this.qtdJogadores = qtdJogadores
+            this.dificuldade = dificuldade
+            this.fases = fases
+            this.fasesConcluidas = fasesConcluidas ?? 0
+            this.zerado = zerado ?? false
+        }
+}
+
+const meuJogo = new Jogo(
+    "Escola", "Terror", 45, 5, 3, 2, false
+)
+
+console.log(meuJogo)
+
+export default Jogo
