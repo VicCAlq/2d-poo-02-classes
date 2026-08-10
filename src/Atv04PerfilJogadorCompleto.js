199858ff31123db7acbffba48f81d36268c6d483
Arthur <arthur.machado@nave.org.br>
2026-08-10 14:02:32 -0300

    Implement PerfilJogador class with methods

--- src/Atv04PerfilJogadorCompleto.js
+++ src/Atv04PerfilJogadorCompleto.js
@@ -13,3 +13,58 @@
  * Lembre-se de exportar a classe "PerfilJogador" no final do arquivo usando
  * "export default PerfilJogador"
  */
+
+class PerfilJogador {
+  constructor(
+    jogador, email, jogos, amigos
+  ) {
+    if (typeof(jogador) !== "string" 
+        || typeof(email) !== "string"
+        || typeof(jogos) !== Array.isArray(jogos)
+        || typeof(amigos) !== Array.isArray(amigos)
+        ){
+            throw new Error("Tipo errado boy")
+        }
+            this.jogador = jogador
+            this.email = email
+            this.jogos = jogos
+            this.amigos = amigos
+            }
+
+    tempoJogador(){
+        let soma = 0
+        let i = 0
+        while (i <= this.jogos.length()){
+            soma += this.jogos[i].fasesConcluidas * this.jogos[i].dificuldade * 10;
+            i++
+        }
+        return soma
+    }
+
+    zerados(){
+        let quant = 0
+        let i = 0
+        while (i <= this.jogos.length()){
+            if (this.jogos[i].zerado == true){
+           quant++ 
+            }
+            i++
+        }
+        return quant
+    }
+}
+const perfilJogador = new PerfilJogador(
+    "Rurthraa", "Arthurmachadocorreia0w0@gmail.com", [], []
+)
+const jogadorAmigo = new PerfilJogador(
+    "Rurthraa", "Arthurmachadocorreia0w0@gmail.com", [] 
+)
+perfilJogador.amigos.push(jogadorAmigo)
+
+
+const j1 = new Jogo()
+perfilJogador.jogos.push(j1)
+
+console.log(PerfilJogador.jogar())
+export default PerfilJogador
+
