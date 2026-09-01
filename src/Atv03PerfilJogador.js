804c8d72cce151ebcde31cb4f88c23e1026daa4d
Kaylane-Marinho-nave <kaylane.marinho@nave.org.br>
2026-09-01 13:31:39 -0300

    fiz a 3

--- src/Atv03PerfilJogador.js
+++ src/Atv03PerfilJogador.js
@@ -12,3 +12,42 @@
  * Lembre-se de exportar a classe "PerfilJogador" no final do arquivo usando
  * "export default PerfilJogador"
  */
+class PerfilJogador 
+{
+  constructor(
+    jogador, email, jogos, amigos
+  ) { 
+
+    if (typeof(jogador) !== "string" 
+        || typeof(email) !== "string"
+        || typeof(jogos) !== Array.isArray(jogos)
+        || typeof(amigos) !== Array.isArray(amigos)
+
+        ){
+            throw new Error("Errado cara")
+
+        }
+            this.jogador = jogador
+            this.email = email
+            this.jogos = jogos
+            this.amigos = amigos
+    }
+}
+
+
+const perfilJogador = new PerfilJogador(
+    "kaykay", "kaylanemarinho17@gmail.com", [], []
+)
+const jogadorAmigo = new PerfilJogador(
+    "Kaykay", "kaylanemarinho17@gmail.com", [] 
+)
+perfilJogador.amigos.push(jogadorLegal)
+
+
+
+
+const j1 = new Jogo()
+perfilJogador.jogos.push(j1)
+
+console.log(PerfilJogador.jogar())
+export default PerfilJogador
